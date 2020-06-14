6a275e967ac8d7ffa82c391cdc7ac087d9806f97
Christine Theobald <christinetheobald@icloud.com>
2020-06-14 17:43:30 -0400

    added images and links

--- src/mock/data.js
+++ src/mock/data.js
@@ -19,17 +19,17 @@ export const heroData = {
 export const aboutData = {
   img: 'profile.jpeg',
   paragraphOne: 'Full Stack Developer',
-  paragraphTwo: 'Building Interactive Web Apps',
+  paragraphTwo: 'Building Interactive Applications',
   paragraphThree:
     'Skilled in Front End and Back End development.  Experience with Java, Javascript, React, Python, Django',
-  resume: 'https://www.resumemaker.online/es.php', // if no resume, the button will not show up
+  resume: '', // if no resume, the button will not show up
 };
 
 // PROJECTS DATA
 export const projectsData = [
   {
     id: uuidv1(),
-    img: 'project.jpg',
+    img: 'robot.jpg',
     title: 'RoboFriends',
     info: 'React App built with Create-React-App',
     info2: 'Uses Search Tool to filter Robots by Name.',
@@ -38,53 +38,53 @@ export const projectsData = [
   },
   {
     id: uuidv1(),
-    img: 'project.jpg',
+    img: 'math.jpg',
     title: 'Math Flashcard App',
     info: 'Addition, Subtraction, Multiplication, and Division Flashcard web app.',
     info2: 'Built with Python and Django. Deployed with Heroku.',
     url: 'https://ctflashcardapp.herokuapp.com/',
     repo: 'https://github.com/CTheobald/django_flashcards', // if no repo, the button will not show up
   },
-  {
-    id: uuidv1(),
-    img: 'project.jpg',
-    title: '',
-    info: '',
-    info2: '',
-    url: '',
-    repo: 'https://github.com/cobidev/react-simplefolio', // if no repo, the button will not show up
-  },
+  // {
+  //   id: uuidv1(),
+  //   img: 'project.jpg',
+  //   title: '',
+  //   info: '',
+  //   info2: '',
+  //   url: '',
+  //   repo: 'https://github.com/cobidev/react-simplefolio', // if no repo, the button will not show up
+  // },
 ];
 
 // CONTACT DATA
 export const contactData = {
   cta: '',
   btn: '',
-  email: '',
+  email: 'chtheobald@gmail.com',
 };
 
 // FOOTER DATA
 export const footerData = {
   networks: [
-    {
-      id: uuidv1(),
-      name: 'twitter',
-      url: '',
-    },
-    {
-      id: uuidv1(),
-      name: 'codepen',
-      url: '',
-    },
+    // {
+    //   id: uuidv1(),
+    //   name: 'twitter',
+    //   url: '',
+    // },
+    // {
+    //   id: uuidv1(),
+    //   name: 'codepen',
+    //   url: '',
+    // },
     {
       id: uuidv1(),
       name: 'linkedin',
-      url: '',
+      url: 'https://www.linkedin.com/in/christine-theobald-a63286b3/',
     },
     {
       id: uuidv1(),
       name: 'github',
-      url: '',
+      url: 'https://github.com/CTheobald',
     },
   ],
 };
